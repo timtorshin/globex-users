58f729d911b114edcd632e3a0d2b91553ce111c9
Timofei Torshin <timtorshin@gmail.com>
2022-10-17 15:00:42 +0300

    feat: implemented search for cards sorted by name

--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -5,6 +5,7 @@ import searchIcon from '../../assets/search-icon.png';
 
 const CardList = () => {
   const [items, setItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
 
   const getItems = () => {
     fetch('http://127.0.0.1:3000')
@@ -14,6 +15,10 @@ const CardList = () => {
       })
       .catch((error) => console.log(error));
   };
+
+  const filteredItems = items.filter((item) => {
+    return item.name.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
   React.useEffect(() => {
     getItems();
@@ -25,12 +30,13 @@ const CardList = () => {
         <input
           className="cards__search-input"
           type="text"
+          onChange={(evt) => setSearchValue(evt.target.value)}
         />
         <img className="cards__search-icon" src={searchIcon} alt="SearchIcon" />
       </form>
       <div className="cards__list">
         {
-          items.map((item, index) => (
+          filteredItems.map((item, index) => (
             <CardItem value={item} key={index} />
           ))
         }
